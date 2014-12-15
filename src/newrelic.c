67a54318ee7f61c40086037255c28c10ae8f0d70
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-12-15 14:03:46 +0000

    Removes test methods.

--- src/newrelic.c
+++ src/newrelic.c
@@ -3,14 +3,10 @@
 #include "newrelic_transaction.h"
 #include "newrelic_collector_client.h"
 
-static ERL_NIF_TERM power_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+static ERL_NIF_TERM transaction_begin_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-  int x, ret;
-  if (!enif_get_int(env, argv[0], &x)) {
-    return enif_make_badarg(env);
-  }
-  ret = x * x;
-  return enif_make_int(env, ret);
+  long transaction_id = newrelic_transaction_begin();
+  return enif_make_int(env, transaction_id);
 }
 
 static ERL_NIF_TERM begin_transaction_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
@@ -20,8 +16,7 @@ static ERL_NIF_TERM begin_transaction_nif(ErlNifEnv* env, int argc, const ERL_NI
 }
 
 static ErlNifFunc nif_funcs[] = {
-  {"power", 1, power_nif},
-  {"begin_transaction", 0, begin_transaction_nif}
+  {"transaction_begin", 0, transaction_begin_nif}
 };
 
 ERL_NIF_INIT(newrelic, nif_funcs, NULL, NULL, NULL, NULL)
--- src/newrelic.erl
+++ src/newrelic.erl
@@ -1,5 +1,5 @@
 -module(newrelic).
--export([power/1, begin_transaction/0]).
+-export([transaction_begin/0]).
 
 -on_load(init/0).
 
@@ -7,8 +7,5 @@ init() ->
   Path = filename:join(code:priv_dir(newrelic), "newrelic"),
   ok = erlang:load_nif(Path, 0).
 
-power(_X) ->
-  exit(nif_library_not_loaded).
-
-begin_transaction() ->
+transaction_begin() ->
   exit(nif_library_not_loaded).
