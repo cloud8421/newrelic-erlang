0d255dccba29fa22a80a2187f6a3b8ea063e7399
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-12-19 17:06:09 +0000

    Adds Transaction set name.

--- src/newrelic.c
+++ src/newrelic.c
@@ -9,14 +9,26 @@ static ERL_NIF_TERM transaction_begin_nif(ErlNifEnv* env, int argc, const ERL_NI
   return enif_make_int(env, transaction_id);
 }
 
-static ERL_NIF_TERM begin_transaction_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+/* transaction_id::long, name::char */
+static ERL_NIF_TERM transaction_set_name_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-  long transaction_id = newrelic_transaction_begin();
-  return enif_make_int(env, transaction_id);
+  long transaction_id;
+
+  if (!enif_get_long(env, argv[0], &transaction_id)) {
+    return enif_make_badarg(env);
+  }
+
+  if (!enif_is_list(env, argv[1])) {
+    return enif_make_badarg(env);
+  }
+
+  newrelic_transaction_set_name(transaction_id, argv[1]);
+  return enif_make_atom(env, "ok");
 }
 
 static ErlNifFunc nif_funcs[] = {
-  {"transaction_begin", 0, transaction_begin_nif}
+  {"transaction_begin", 0, transaction_begin_nif},
+  {"transaction_set_name", 2, transaction_set_name_nif}
 };
 
 ERL_NIF_INIT(newrelic, nif_funcs, NULL, NULL, NULL, NULL)
--- src/newrelic.erl
+++ src/newrelic.erl
@@ -1,5 +1,5 @@
 -module(newrelic).
--export([transaction_begin/0]).
+-export([transaction_begin/0, transaction_set_name/2]).
 
 -on_load(init/0).
 
@@ -8,4 +8,7 @@ init() ->
   ok = erlang:load_nif(Path, 0).
 
 transaction_begin() ->
+  exit(nif_library_not_loaded).
+
+transaction_set_name(_Id, _Name) ->
   exit(nif_library_not_loaded).
